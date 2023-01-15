ef82a1bbb61685b24d2adcebe4b269e57a288d55
riflowth <riflowth@gmail.com>
2023-01-15 19:23:40 +0700

    feat: initial commit

---
+++ src/components/experiences/ComcampExperience.svelte
@@ -0,0 +1,22 @@
+<div class="flex flex-col">
+  <div class="flex flex-col md:flex-row justify-between mb-2">
+    <div class="flex flex-col">
+      <span class="text-xl text-white font-semibold">Comcamp 33</span>
+      <div class="flex flex-col md:flex-row md:items-center md:space-x-2">
+        <span class="md:text-lg text-neutral-400 font-semibold">Software Development Consultant & DevOps Engineer</span>
+        <span class="text-sm text-neutral-400 font-light">01.2022 - 04.2022 / BKK, TH</span>
+      </div>
+    </div>
+    <div class="flex flex-row items-center space-x-2 mt-2 md:mt-0">
+      <span class="text-neutral-400 font-semibold">Backers</span>
+      <div class="flex flex-row items-center space-x-2">
+        <img src="/logo/vectier-white-with-bg.svg" alt="" class="w-8 h-8 rounded-md" />
+        <img src="/logo/kmutt-white-with-bg.svg" alt="" class="w-8 h-8 rounded-md" />
+      </div>
+    </div>
+  </div>
+
+  <span class="text-neutral-400 mb-6">
+    An interactive real-time web application for the activity of CPE PRE-FRESHY CPE 2021 under the name of King Mongkut's University of Technology Thonburi (KMUTT).
+  </span>
+</div>
---
+++ src/components/experiences/Experiences.svelte
@@ -0,0 +1,17 @@
+<script>
+  import CodernExperience from '@/components/experiences/CodernExperience.svelte';
+  import TheExistingExperience from '@/components/experiences/TheExistingExperience.svelte';
+  import PrefreshyCpeExperience from '@/components/experiences/PrefreshyCpeExperience.svelte';
+  import ComcampExperience from '@/components/experiences/ComcampExperience.svelte';
+</script>
+
+<section id="experience" class="flex flex-col py-24">
+  <span class="mb-10 text-2xl text-white font-semibold">Experience</span>
+
+  <div class="space-y-4">
+    <CodernExperience />
+    <TheExistingExperience />
+    <ComcampExperience />
+    <PrefreshyCpeExperience />
+  </div>
+</section>
